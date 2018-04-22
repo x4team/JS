030b4d4904bbc4723c627b595cf976ad1e8e8d19
Dmitry <x4team@gmail.com>
2018-04-22 23:24:10 +0300

    Modified learn13

--- learn12/code.js
+++ learn12/code.js
@@ -1,17 +1,45 @@
 var message = prompt("Please enter message for encryption: \n *The text should be simple. A space is denoted by a semicolon  ; ");
+
 var encryption = [];
+
 var temp_symbol = 0;
+
 for (var i = 0; i < message.length; i++)
+
    {
-	//Шифруем сообещния
+
+//Шифруем сообщения
+
     temp_symbol = message[i].charCodeAt(0) * 234;
+
     //Добавляем зашифрованный символ в массив encryption
+
     encryption[i] = temp_symbol;
+
     temp_symbol = 0;
 
    }
 
 document.write(encryption);
+
+var messageOriginal=[];
+messageOriginal=encryption;
+var deencryption=[];
+for (var i = 0; i < messageOriginal.length; i++)
+
+   {
+
+//Дешифруем сообщения
+
+    temp_symbol = String.fromCharCode(messageOriginal[i] / 234);
+    
+    //Добавляем расшифрованный символ в массив deencryption
+deencryption[i] = temp_symbol;
+
+   }
+   var str = deencryption.join('');
+
+document.write("<br/>" + str);
 
 
 
