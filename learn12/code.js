6e92872ce08d440a822f369e53ee3cf6fdeac5a0
Dmitry Rakov <x4team@gmail.com>
2018-04-24 18:46:43 +0300

    code completion

--- learn12/code.js
+++ learn12/code.js
@@ -1,16 +1,33 @@
 var message = prompt("Please enter message for encryption: \n *The text should be simple. A space is denoted by a semicolon  ; ");
 
+
+//Вводим ключ шифрования. Обязательно не менее и не более 8 символов.
+var key_encrypt_input = prompt("Please enter KEY for encryption: \n *Strictly ONLY 8 characters ");
+var key_encrypt = [];
+
+//Проверяем если пользователь вводит не по шаблону
+while (key_encrypt_input.length<8||key_encrypt_input.length>8) {
+key_encrypt_input = prompt("Please enter KEY for encryption: \n *Strictly ONLY 8 characters "); 
+}  
+
+//Если условие выполнено - запоминаем
+var key_encrypt=key_encrypt_input; 
+
 var encryption = [];
 
 var temp_symbol = 0;
 
+//Алгоритм ключа шифрования
+
+var key_encrypt_method = (key_encrypt[0].charCodeAt(0) + key_encrypt[1].charCodeAt(0) + key_encrypt[2].charCodeAt(0) + key_encrypt[3].charCodeAt(0)) * key_encrypt[4].charCodeAt(0) + (key_encrypt[5].charCodeAt(0) * key_encrypt[6].charCodeAt(0) * key_encrypt[7].charCodeAt(0));
+
+
+//Цикл шифрования
 for (var i = 0; i < message.length; i++)
 
    {
 
-//Шифруем сообщения
-
-    temp_symbol = message[i].charCodeAt(0) * 234;
+   temp_symbol = message[i].charCodeAt(0) * key_encrypt_method; //Алгоритм шифрования сообщения
 
     //Добавляем зашифрованный символ в массив encryption
 
@@ -22,6 +39,8 @@ for (var i = 0; i < message.length; i++)
 
 document.write(encryption);
 
+
+//Дешифруем
 var messageOriginal=[];
 messageOriginal=encryption;
 var deencryption=[];
@@ -31,7 +50,7 @@ for (var i = 0; i < messageOriginal.length; i++)
 
 //Дешифруем сообщения
 
-    temp_symbol = String.fromCharCode(messageOriginal[i] / 234);
+    temp_symbol = String.fromCharCode(messageOriginal[i] / key_encrypt_method);
     
     //Добавляем расшифрованный символ в массив deencryption
 deencryption[i] = temp_symbol;
--- learn9/code.js
+++ learn9/code.js
@@ -1,31 +1,32 @@
-var score = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 
 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
-var tests = score.length;
 var HighestScore = 0;
-var i = 0;
 var output;
 
 //Цикл выводит все значения массива score
-for (var i = 0, i < score.length, i++) {
-   output = "<br>Bubble solution #" + i + ": " + "score: " + score[i];
+for (var i = 0; i < scores.length; i++) {
+   output = "<br>Bubble solution #" + i + " score: " + scores[i];
    document.write(output);
    //Выполняем условие для поиска самого большого значения в массиве
-   if (score[i] >= HighestScore) { HighestScore = score[i] };
+   if (scores[i] > HighestScore) { HighestScore = scores[i] };
 	
 }
-i = 0;
 
 
-document.write ("<br><br>Bubbles tests: " + tests);
+document.write ("<br><br>Bubbles tests: " + scores.length);
 document.write ("<br>Highest bubble score: " + HighestScore);
-document.write ("<br>Solutions with highest score: "); 
-GetHighestScore();
 
-//Создаем функцию, которая будет искать в массиве элементы равные HighestScore
-function GetHighestScore() {
-		while (i < score.length) {
-			if (score[i]==HighestScore) { document.write("#" + i + ", ") };
-            i++;
+
+
+var HighestScoreArray = [];
+//Ищем в массиве элементы равные HighestScore, и записываем значения в массив HighestScoreArray
+
+		for (var i = 0; i < scores.length; i++) {
+			if (scores[i] == HighestScore) { 
+        HighestScoreArray.push(i); //Функция push работает с массивом данных, добавляет значения в конец массива
+      }
+        
 		}
-     }; 
+     
+document.write ("<br>Solutions with highest score: " + HighestScoreArray); 
